fe4874f793116bc1a53acf038bbb6a1d83401154
Rebecca Lewis <rvicknair21@hotmail.com>
2019-05-26 22:45:17 -0500

    Remove assignment 11_1

--- Assignment_11_1_LewisRebecca.py
+++
@@ -1,9 +0,0 @@
-#File: Assignment_11_1_LewisRebecca.py
-#Name: Rebecca Lewis
-#Date: May 26, 2019
-#Course: DSC 510
-#Usage: Build a cash register to allow a user to add items and calculate a total using a class
-
-#Changing to test with push/pull in github
-print("Hello World")
-print("Testing Push/pull throgh git command line")
